d3eff6f06a5989eb0fe6dc8c678ec2988159eec4
Benedict <benedictryan80@gmail.com>
2022-11-07 03:40:34 +0700

    create sign up feature

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,8 +1,23 @@
 import { getSession, signIn, signOut } from 'next-auth/react';
+import { useState } from 'react';
 
 function SignIn() {
+	const [userInfo, setUserInfo] = useState({ email: '', password: '' });
+
 	const handleGoogleSignIn = async () => {
 		await signIn('google', { callbackUrl: '/products' });
+	};
+
+	const handleSignUp = async () => {
+		console.log(userInfo);
+		const res = await fetch('/api/create-user', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(userInfo)
+		});
+		res = await res.json();
+		console.log(res);
+		setUserInfo({ email: '', password: '' });
 	};
 
 	return (
@@ -15,16 +30,26 @@ function SignIn() {
 		>
 			<h1>Sign in</h1>
 			<br />
-			<input type="email" placeholder="email" />
+			<input
+				type="email"
+				placeholder="email"
+				value={userInfo.email}
+				onChange={(e) => setUserInfo({ ...userInfo, email: e.target.value })}
+			/>
 			<br />
 			<br />
-			<input type="password" placeholder="password" />
+			<input
+				type="password"
+				placeholder="password"
+				value={userInfo.password}
+				onChange={(e) => setUserInfo({ ...userInfo, password: e.target.value })}
+			/>
 			<br />
 			<br />
 			<button>Sign in</button>
 			<br />
 			<br />
-			<button>Sign up</button>
+			<button onClick={handleSignUp}>Sign up</button>
 			<br />
 			<br />
 			<button onClick={handleGoogleSignIn}>Google Sign In</button>
