7a356ec7b4b65eea5cfa5ccf213311d0a98050a3
Benedict <benedictryan80@gmail.com>
2022-11-07 16:46:09 +0700

    add dynamic crud feature 2

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -4,19 +4,11 @@ export default async function handler(req, res) {
 	const client = await clientPromise;
 	const db = client.db('test');
 	const products = db.collection('products');
-	const { body, method } = req;
+	const { body } = req;
 
-	if (method === 'GET') {
-		console.log(body);
-		const allProducts = await products
-			.find({ author: { email: 'alanturing@gmail.com' } })
-			.toArray();
-		res.json({ status: 200, data: allProducts });
-	} else if (method === 'POST') {
-		let bodyObject = JSON.parse(body);
-		// console.log(bodyObject);
+	let bodyObject = JSON.parse(body);
+	// console.log(bodyObject);
 
-		const newProduct = await products.insertOne(bodyObject);
-		res.json({ status: 200, data: newProduct });
-	}
+	const newProduct = await products.insertOne(bodyObject);
+	res.json({ status: 200, data: newProduct });
 }
